3a5ff90b50f8f38d980a0030b004511c5a2168c1
AyalaVirtual <erica.ayala.work@gmail.com>
2023-09-27 14:48:16 -0400

    Complete Book model with constructors, getters and setters, and toString method

--- src/main/java/com/example/authorbookapi/model/Book.java
+++ src/main/java/com/example/authorbookapi/model/Book.java
@@ -31,10 +31,68 @@ public class Book {
     private Author author;
 
 
+    public Book() {
+    }
+
+    public Book(Long id, String name, String description, String isbn, Author author) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.isbn = isbn;
+        this.author = author;
+    }
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
 
 
-
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", author=" + author +
+                '}';
+    }
 
 }
