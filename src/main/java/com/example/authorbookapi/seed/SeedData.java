f406a0e4a0d75034b49419cbab7a95ca0d38f561
AyalaVirtual <erica.ayala.work@gmail.com>
2023-09-27 15:40:41 -0400

    Complete seed data by adding attributes for each model's repository, creating constructor, and saving model instances to corresponding repostory

--- src/main/java/com/example/authorbookapi/seed/SeedData.java
+++ src/main/java/com/example/authorbookapi/seed/SeedData.java
@@ -2,17 +2,25 @@ package com.example.authorbookapi.seed;
 
 import com.example.authorbookapi.model.Author;
 import com.example.authorbookapi.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import com.example.authorbookapi.repository.AuthorRepository;
+import com.example.authorbookapi.repository.BookRepository;
 
 
 @Component
 public class SeedData implements CommandLineRunner {
 
-    // Add attributes representing model repositories with 'final' modifier
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
 
 
-    // Autowire constructor utilizing model repository attributes
+    @Autowired
+    public SeedData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
 
 
     @Override
@@ -23,7 +31,7 @@ public class SeedData implements CommandLineRunner {
         Author author = new Author();
         author.setFirstName("George R.R.");
         author.setLastName("Martin");
-        // save author instance to author repository
+        authorRepository.save(author);
 
 
         Book book1 = new Book();
@@ -31,7 +39,7 @@ public class SeedData implements CommandLineRunner {
         book1.setName("Fire & Blood");
         book1.setDescription("Long before the events in A Game of Thrones, House Targaryen, the sole surviving dragonlord lineage post-Valyria's destruction, made Dragonstone their home. Fire and Blood commences with Aegon the Conqueror, the Iron Throne's founder, and proceeds to chronicle generations of Targaryen struggles for dominance, culminating in a perilous civil conflict.");
         book1.setIsbn("8675309");
-        // save book instance to book repository
+        bookRepository.save(book1);
 
     }
 }
