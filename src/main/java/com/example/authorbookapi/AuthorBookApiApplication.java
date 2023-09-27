ed2f809967ad446975419eab3dc5607327a296ce
AyalaVirtual <erica.ayala.work@gmail.com>
2023-09-27 12:31:57 -0400

    Initialize project

---
+++ src/main/java/com/example/authorbookapi/AuthorBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.authorbookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorBookApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorBookApiApplication.class, args);
+    }
+
+}
