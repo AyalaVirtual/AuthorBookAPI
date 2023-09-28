019a40603ea7fba892821a274a08252c2f949bc6
AyalaVirtual <erica.ayala.work@gmail.com>
2023-09-27 20:20:47 -0400

    Edit getFullName method in author model and add fullName as attribute. Add commented out psuedocode to GET and POST methods for book model in service class

--- src/main/java/com/example/authorbookapi/service/AuthorService.java
+++ src/main/java/com/example/authorbookapi/service/AuthorService.java
@@ -3,6 +3,7 @@ package com.example.authorbookapi.service;
 import com.example.authorbookapi.exception.InformationExistException;
 import com.example.authorbookapi.exception.InformationNotFoundException;
 import com.example.authorbookapi.model.Author;
+import com.example.authorbookapi.model.Book;
 import com.example.authorbookapi.repository.AuthorRepository;
 import com.example.authorbookapi.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -93,15 +94,27 @@ public class AuthorService {
 
 
     // GET all books (?? use optional ?? - check repository if list is empty -> throw not found exception / return find all from repository ??)
+    public Optional<List<Book>> getAllBooks() {
 
+        // Create optional of author?? Search author's (repository ?? Create custom findAllBooks method in repository returns author's bookList ) bookList for this book by id. If
+
+    }
 
 
     // GET individual book by id (use optional - check repository by id if present -> return optional / throw not found exception)
+    public Optional<Book> getBookById() {
 
+        // Create optional of author?? Search author's (repository ?? Create custom findBookById method in repository returns author's bookList ??) bookList for this book by id. If
+
+    }
 
 
     // POST (create) book (check repository by name if exists [!= null] -> throw exists exception / return save to repository)
+    public Book createBook() {
 
+        // Books must be added/saved to author's bookList (in repository ?? ---> " if (optional.isPresent()) { list.add(optional.get()) } ").
+
+    }
 
 
     // PUT (update) existing book (use optional - check repository by id if present -> set attributes, save to repository, return optional / throw not found exception)
