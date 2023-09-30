a4c646f792b18392381e193bfa3410bed58bc953
AyalaVirtual <erica.ayala.work@gmail.com>
2023-09-29 20:08:36 -0400

    Add commented out psuedocode for testing POST method for author model.

--- src/test/java/com/example/authorbookapi/controller/AuthorControllerTest.java
+++ src/test/java/com/example/authorbookapi/controller/AuthorControllerTest.java
@@ -67,8 +67,7 @@ public class AuthorControllerTest {
 
 
     // When calling authorService, get author by id using the author's record to call the getID method. Then return an optional of the author's record.
-    // Use mockMvc to perform a GET request to the endpoint and uri variable (which is the id, so it would be "/api/authors/{id}/", "1") using MockMvcRequestBuilders. Set the content type you're expecting, which is MediaType.APPLICATION_JSON. And expect the (response) status is ok. And expect the jsonPath of the 'data.id' key of the payload, and the value of id of the author's record (RECORD_1.getId()). And expect the jsonPath of each of the model's attributes (so the 'data.firstName' key of the payload, and the value of firstName of the author's record (RECORD_1.getFirstName()), then repeat from 'And expect the jsonPath' for the 'data.lastName' key of the payload, and the value of lastName of the author's record (RECORD_1.getLastName()).
-    // And expect the jsonPath of the 'message' key of the payload to have a value of 'success'. And do print (the message).
+    // Use mockMvc to perform a GET request to the endpoint and uri variable (which is the id, so it would be "/api/authors/{id}/", "1") using MockMvcRequestBuilders. Set the content type you're expecting, which is MediaType.APPLICATION_JSON. And expect the (response) status is ok. And expect the jsonPath of the 'data.id' key of the payload, and the value of id of the author's record (RECORD_1.getId()). And expect the jsonPath of each of the model's attributes (so the 'data.firstName' key of the payload, and the value of firstName of the author's record (RECORD_1.getFirstName()), then repeat from 'And expect the jsonPath' for the 'data.lastName' key of the payload, and the value of lastName of the author's record (RECORD_1.getLastName()). And expect the jsonPath of the 'message' key of the payload to have a value of 'success'. And do print (the message).
     @Test // GET /api/authors/1/
     public void getAuthorRecord_success() throws Exception {
 
@@ -86,6 +85,14 @@ public class AuthorControllerTest {
 
 
     @Test // POST /api/authors/
+    // When calling authorService, create author using 'Mockito.any(Author.class)'. Then return the author's record.
+    // Create a mock request using MockHttpServletRequestBuilder and set it equal to calling a POST request to the endpoint ("/api/authors/") from MockMvcRequestBuilders. Set the content type you're expecting, which is 'MediaType.APPLICATION_JSON'. Accept 'MediaType.APPLICATION_JSON'. Use 'this.objectMapper' (used to convert the Java object to JSON and vice versa) to write the value of the author's record as a string ( writeValueAsString(RECORD_1) ).
+    // Use mockMvc to perform the mock request. And expect the (response) status is created. And expect the jsonPath of the payload, and a not null value. And expect the jsonPath of the 'data.id' key of the payload, and the value of id of the author's record (RECORD_1.getId()). And expect the jsonPath of each of the model's attributes (so the 'data.firstName' key of the payload, and the value of firstName of the author's record (RECORD_1.getFirstName()), then repeat from 'And expect the jsonPath' for the 'data.lastName' key of the payload, and the value of lastName of the author's record (RECORD_1.getLastName()). And expect the jsonPath of the 'message' key of the payload to have a value of 'success'. And do print (the message).
+    public void createAuthorRecord_success() throws Exception {
+
+
+
+    }
 
 
 
