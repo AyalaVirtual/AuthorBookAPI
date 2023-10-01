8238bb91a92c33b3cff375909130fc3bb4d9c3c8
AyalaVirtual <erica.ayala.work@gmail.com>
2023-10-01 13:30:43 -0400

    Create definitions and runners packages. Create and complete TestRunner class. Begin step definitions.

---
+++ src/test/java/definitions/AuthorControllerTestDefinitions.java
@@ -0,0 +1,44 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+
+public class AuthorControllerTestDefinitions {
+    @Given("A list of authors are available")
+    public void aListOfAuthorsAreAvailable() {
+    }
+
+    @When("I add an author to my list")
+    public void iAddAnAuthorToMyList() {
+    }
+
+    @Then("The author is added")
+    public void theAuthorIsAdded() {
+    }
+
+    @When("I get a specific author")
+    public void iGetASpecificAuthor() {
+    }
+
+    @Then("The specific author is available")
+    public void theSpecificAuthorIsAvailable() {
+    }
+
+    @When("I edit an author from my list")
+    public void iEditAnAuthorFromMyList() {
+    }
+
+    @Then("The author content is edited")
+    public void theAuthorContentIsEdited() {
+    }
+
+    @When("I remove author from my list")
+    public void iRemoveAuthorFromMyList() {
+    }
+
+    @Then("The author is removed")
+    public void theAuthorIsRemoved() {
+    }
+}
